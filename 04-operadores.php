36fbc99d7c3cacb50624c50141404bedc0079dcc
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-18 18:53:08 -0500

    operadores

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,5 +1,23 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
+
+// Suma
+echo $numero1 + $numero2;
+echo "<br>";
+
+// Resta
+echo $numero1 - $numero2;
+echo "<br>";
+
+// Multiplicar
+echo $numero1 * $numero2;
+echo "<br>";
+
+// Dividir
+echo $numero1 / $numero2;
+echo "<br>";
 
 
 
