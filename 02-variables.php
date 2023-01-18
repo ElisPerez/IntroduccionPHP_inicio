9dee1d0e7d3f7f2f368241dd2a04cd2b176e9557
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-18 18:37:48 -0500

    variables en php

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,21 @@
 <?php include 'includes/header.php';
 
+$nombre = "Antonio";
+$_apellido = "Perez";
+$nombreCliente = "Pedro";
+$apellido_cliente = "Torres";
 
+echo $nombre, " ", $_apellido;
 
+var_dump($nombre);
+
+// Constantes en PHP
+define('nombreConstante', "Este es el valor de la constante");
+
+echo nombreConstante;
+
+// Segunda forma de crear constantes en php:
+const nombreConstante2 = "Valor de la constante 2";
+echo nombreConstante2;
 
 include 'includes/footer.php';
