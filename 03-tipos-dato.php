4e6a913521f2fb3bb85ca9ab0bf2107dee8634ee
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-18 18:45:25 -0500

    tipos de datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,24 @@
 <?php include 'includes/header.php';
 
+// Boolean
+$logueado = true;
+var_dump($logueado);
 
+// Enteros
+$numero_entero = 200;
+var_dump($numero_entero);
+
+// Float
+$numeroFloat = 30.5;
+var_dump($numeroFloat);
+
+// String
+$nombre = "Antonio";
+var_dump($nombre);
+
+// Array
+$miArreglo = [];
+var_dump($miArreglo);
 
 
 include 'includes/footer.php';
