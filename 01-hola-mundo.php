e2fdf4041f814fc8aec77e6d8acc9bdd0e10c13d
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-19 13:13:22 -0500

    array asociativo como los objetos literales de javascript

--- 01-hola-mundo.php
+++ 01-hola-mundo.php
@@ -1,12 +1,12 @@
 <?php include 'includes/header.php';
-// echo "<h1>Hola mundo</h1>";
-// echo ("<h1>Hola mundo</h1>");
+echo "<h1>Hola mundo</h1>";
+echo ("<h1>Hola mundo</h1>");
 
-// print "<h1>Hola mundo</h1>";
-// print ("<h1>Hola mundo</h1>");
-// print_r("<h1>Hola mundo</h1>");
+print "<h1>Hola mundo</h1>";
+print ("<h1>Hola mundo</h1>");
+print_r("<h1>Hola mundo</h1>");
 
-// var_dump('<h1>Hola mundo</h1>');
-// var_dump(20);
+var_dump('<h1>Hola mundo</h1>');
+var_dump(20);
 
 include 'includes/footer.php';
--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,40 @@
 <?php include 'includes/header.php';
 
+/* ----- Como los objetos en JS ----- */
+$cliente = [
+    'nombre' => 'Antonio',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium',
+        'disponible' => 100
+    ],
+];
 
+echo '<pre>';
+var_dump($cliente);
+echo '</pre>';
+echo '<br>';
+
+/* ---- Acceder a un a propiedad --- */
+echo '<pre>';
+var_dump($cliente['informacion']);
+echo '</pre>';
+echo '<br>';
+
+echo $cliente['informacion']['tipo'];
+echo '<br>';
+
+echo '<pre>';
+var_dump($cliente['nombre']);
+echo '</pre>';
+echo '<br>';
+
+echo $cliente['saldo'];
+echo '<br>';
+
+/* -------- Agregar propiedad ------- */
+$cliente['codigo'] = 123456;
+echo $cliente['codigo'];
 
 
 include 'includes/footer.php';
