d278667d8553eed27e504704842b4d705be23f78
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-19 13:28:08 -0500

    empty and isset

--- 10-isset-empty.php
+++ 10-isset-empty.php
@@ -1,6 +1,34 @@
 <?php include 'includes/header.php';
 
+$clientes = [];
+$clientes2 = array();
+$clientes3 = ['Antonio', 'John', 'Martha'];
+$clientes5 = [
+  'nombre' => 'Antonio',
+  'saldo' => 200
+];
 
+/* -------------- Empty ------------- */
+// Revisa si un array está vacío
+var_dump(empty($clientes));
+echo "<br>";
+var_dump(empty($clientes3));
+echo "<br>";
 
+/* -------------- Isset ------------- */
+// Revisa si un array está creado o una propiedad está definida
+var_dump(isset($clientes4)); // No está definido entonces da "false"
+echo "<br>";
+var_dump(isset($clientes)); // true
+echo "<br>";
+var_dump(isset($clientes2)); // true
+echo "<br>";
+var_dump(isset($clientes3)); // true
+echo "<br>";
+// revisando propiedades
+var_dump(isset($clientes5['nombre'])); // true
+echo "<br>";
+var_dump(isset($clientes5['codigo'])); // false
+echo "<br>";
 
 include 'includes/footer.php';
