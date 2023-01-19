37fda63d322f5d88d08ec77f6754a7884fced087
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-18 21:38:59 -0500

    incremento decremento

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,61 @@
 <?php include 'includes/header.php';
 
+/* ----------- Incremento ----------- */
+$numero1 = 30;
+$numero1++;
 
+echo $numero1;
+echo "<br>";
+
+$numero1 = 30;
+++$numero1;
+
+echo $numero1;
+echo "<br>";
+
+$numero1 = 30;
+
+echo ++$numero1;
+echo "<br>";
+
+$numero1 = 30;
+
+echo $numero1++;
+echo "<br>";
+
+/* ----------- Decremento ----------- */
+$numero2 = 10;
+$numero2--;
+
+echo $numero2;
+echo "<br>";
+
+$numero2 = 10;
+--$numero2;
+
+echo $numero2;
+echo "<br>";
+
+$numero2 = 10;
+
+echo --$numero2;
+echo "<br>";
+
+$numero2 = 10;
+
+echo $numero2--;
+echo "<br>";
+
+// Incremento personalizado
+$numero3 = 50;
+$numero3 += 5;
+echo $numero3;
+echo "<br>";
+
+// Decremento personalizado
+$numero4 = 60;
+$numero4 -= 2;
+echo $numero4;
 
 
 include 'includes/footer.php';
