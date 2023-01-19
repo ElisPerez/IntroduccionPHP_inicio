074e9a2d1dc411e772d3b07eb989ee075f044650
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-18 19:22:57 -0500

    comparaciones

--- 04-operadores.php
+++ 04-operadores.php
@@ -19,6 +19,9 @@ echo "<br>";
 echo $numero1 / $numero2;
 echo "<br>";
 
+// Multiplicar cierta cantidad de veces
+echo 2 ** 3;
+
 
 
 include 'includes/footer.php';
--- 05-comparacion.php
+++ 05-comparacion.php
@@ -1,5 +1,44 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 30;
+$numero3 = 30;
+$numero4 = "30";
+
+var_dump($numero1 > $numero2);
+echo "<br>";
+
+var_dump($numero1 < $numero2);
+echo "<br>";
+
+var_dump($numero1 >= $numero2);
+echo "<br>";
+
+var_dump($numero1 <= $numero2);
+echo "<br>";
+
+// Comparador de igualdad
+var_dump($numero2 == $numero3);
+echo "<br>";
+
+var_dump($numero3 == $numero4);
+echo "<br>";
+
+// Conparador estricto
+var_dump($numero3 === $numero4);
+echo "<br>";
+
+// -1 si izquierda es menor
+var_dump($numero1 <=> $numero2);
+echo "<br>";
+
+// 0 si son iguales
+var_dump($numero2 <=> $numero3);
+echo "<br>";
+
+// 1 si izquierda es mayor
+var_dump($numero2 <=> $numero1);
+echo "<br>";
 
 
 
