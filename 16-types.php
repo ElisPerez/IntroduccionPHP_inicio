5c2b21b2ca3a8545fdd4e81cb980ef8745c6c082
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-20 11:59:17 -0500

    tipado

--- 16-types.php
+++ 16-types.php
@@ -1,7 +1,32 @@
-<?php include 'includes/header.php';
+<?php
 
+declare(strict_types=1);
+include 'includes/header.php';
 
+function usuarioAutenticado(bool $autenticado): ?string
+{
+  if ($autenticado) {
+    return "El usuario está autenticado";
+  } else {
+    return null;
+  }
+}
 
+$usuario = usuarioAutenticado(true);
 
+echo $usuario;
+
+function usuarioAutenticado2(bool $autenticado): string|int
+{
+  if ($autenticado) {
+    return "El usuario está autenticado";
+  } else {
+    return 20;
+  }
+}
+
+$usuario2 = usuarioAutenticado2(true);
+
+echo $usuario2;
 
 include 'includes/footer.php';
