6867917cd24342ee59599f3bd122e68428222bc6
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-20 09:36:54 -0500

    typado en php desde la version 7 😱

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,7 +1,15 @@
-<?php include 'includes/header.php';
+<?php
+// Para mostrar errores de tipado 😱😱😱
+declare(strict_types=1);
 
+include 'includes/header.php';
 
+function sumar(int $num1 = 0, int $num2 = 0)
+{
+  echo "<p>" . $num1 + $num2 . "</p>";
+}
 
+sumar(2, 2);
 
 
 include 'includes/footer.php';
