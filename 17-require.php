c941e255dd8d58deecab458ba511d54ed9b96558
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-20 12:17:09 -0500

    include and require

--- 17-require.php
+++ 17-require.php
@@ -1,6 +1,12 @@
-<?php include 'includes/header.php';
+<?php include 'includes/header.php'; // include se usa para templates
+// <?php include('includes/header.php'); // tambien se puede asi pero no se recomienda
+// <?php require 'includes/header.php'; // require se usa para funciones importantes
+// <?php require('includes/header.php'); // tambien se puede asi pero no se recomienda
+// <?php require_once 'includes/header.php'; // si el file ya fue incluido, lo ignora.
 
+require 'funciones.php';
 
+iniciarApp();
 
 
 
