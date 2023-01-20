8274836aab1d9a5ad8fc87008589537f99a9b025
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-20 15:58:28 -0500

    json_decode y json_encode

--- 18.json.php
+++ 18.json.php
@@ -1,8 +1,41 @@
 <?php include 'includes/header.php';
 
+$productos = [
+  [
+    'nombre' => 'Tablet',
+    'precio' => 200,
+    'disponible' => true
+  ],
+  [
+    'nombre' => 'Televisión 24"',
+    'precio' => 300,
+    'disponible' => true
+  ],
+  [
+    'nombre' => 'Monitor Curvo',
+    'precio' => 400,
+    'disponible' => false
+  ]
+];
 
+echo "<pre>";
+var_dump($productos);
+echo "</pre>";
+echo "<br>";
+echo "<br>";
 
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
 
+$json_array = json_decode($json);
 
+echo "<pre>";
+var_dump($json);
+echo "</pre>";
+echo "<br>";
+
+echo "<pre>";
+var_dump($json_array);
+echo "</pre>";
+echo "<br>";
 
 include 'includes/footer.php';
